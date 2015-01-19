eff98e635fe41f1ff654e4ef68611e200ec4e750
Michal Šimonfy <virael.s@gmail.com>
2015-01-19 23:33:12 +0100

    Add internationalization

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ angular.module('app', []).controller('forget', function ($scope) {
 	$scope.view = 'default'; // [default, options, about]
 	$scope.since = localStorage.getItem('since') || 'five_minutes';
 
+	$scope.i18n = function(key) {
+		return chrome.i18n.getMessage(key);
+	}
+
 	// Default settings
 	$scope.defaults = {
 		"history": true,
@@ -21,18 +25,18 @@ angular.module('app', []).controller('forget', function ($scope) {
 	};
 
 	$scope.labels = {
-		"history": "Browsing history",
-		"cookies": "Cookies",
-		"cache": "Browser cache",
-		"downloads": "List of downloads",
-		"formData": "Form data",
-		"passwords": "Passwords",
-		"appcache": "Application cache",
-		"localStorage": "LocalStorage data",
-		"pluginData": "Plugin data",
-		"fileSystems": "Filesystems",
-		"indexedDB": "IndexedDB Databases",
-		"webSQL": "WebSQL Databases"
+		"history": $scope.i18n('item_history'),
+		"cookies": $scope.i18n('item_cookies'),
+		"cache": $scope.i18n('item_cache'),
+		"downloads": $scope.i18n('item_downloads'),
+		"formData": $scope.i18n('item_formData'),
+		"passwords": $scope.i18n('item_passwords'),
+		"appcache": $scope.i18n('item_appcache'),
+		"localStorage": $scope.i18n('item_localStorage'),
+		"pluginData": $scope.i18n('item_pluginData'),
+		"fileSystems": $scope.i18n('item_fileSystems'),
+		"indexedDB": $scope.i18n('item_indexedDB'),
+		"webSQL": $scope.i18n('item_webSQL')
 	};
 
 	$scope.init = function() {
@@ -76,6 +80,13 @@ angular.module('app', []).controller('forget', function ($scope) {
 
 	// Highlight browser icon
 	chrome.runtime.sendMessage({ action: "init" });
+
+	// i18n
+	[].forEach.call(document.querySelectorAll('[i18n]'), function(element) {
+
+		element.innerHTML = chrome.i18n.getMessage(element.getAttribute('i18n'));
+
+	});
 
 	// Handle external links
 	[].forEach.call(document.querySelectorAll('[data-href]'), function(link) {
